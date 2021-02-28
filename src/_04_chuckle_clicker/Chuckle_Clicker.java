4d6088b91d04a1a2b7dc8115797544f018153d1c
root <root@10f929b7e2aa>
2021-02-28 22:29:34 +0000

    chuckle clicker

--- src/_04_chuckle_clicker/Chuckle_Clicker.java
+++ src/_04_chuckle_clicker/Chuckle_Clicker.java
@@ -1,30 +1,54 @@
 package _04_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Chuckle_Clicker {
+import _01_nasty_surprise.Nasty_Surprise;
 
+public class Chuckle_Clicker implements ActionListener {
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton("Joke");
+	JButton button2 = new JButton("Punchline");
+	
+	
 	public static void main(String[] args) {
+		Chuckle_Clicker clicker = new Chuckle_Clicker();
+		clicker.makeButtons();
+	}
 		
-		makeButtons();{
-			JOptionPane.showMessageDialog(null, "Make Buttons");
-		}
-		JFrame frame = new JFrame();
-		frame.setVisible(true);
-		JPanel panel = new JPanel();
-		JButton button1 = new JButton("Joke");
-		JButton button2 = new JButton("Punchline");
+		void makeButtons(){
+			
+		button1.addActionListener(this);
+		button2.addActionListener(this);
 		panel.add(button1);
 		panel.add(button2);
 		frame.add(panel);
 		frame.pack();
-	}
+		frame.setVisible(true);
 
-	private static void makeButtons() {
-		// TODO Auto-generated method stub
+}
 		
-	}
+
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+			if(e.getSource()== button1) {
+				JOptionPane.showMessageDialog(null,"Q: Why did the chicken cross the road?");
+			}
+			
+			if(e.getSource()== button2) {
+				JOptionPane.showMessageDialog(null,"A: To get to the other side");
+			}
+		}
+
+	
 }
