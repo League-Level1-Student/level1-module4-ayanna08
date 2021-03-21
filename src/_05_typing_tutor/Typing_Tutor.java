20131933d0c3111e18e104237dc96d1b6567827b
root <root@a075445fbb22>
2021-03-21 21:30:08 +0000

    fortune teller

--- src/_05_typing_tutor/Typing_Tutor.java
+++ src/_05_typing_tutor/Typing_Tutor.java
@@ -1,7 +1,9 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -44,14 +46,21 @@ public class Typing_Tutor implements KeyListener {
 			
 			if(arg0.getKeyChar() == currentLetter) {
 				JOptionPane.showMessageDialog(null, "Correct");
-				panel.setBackground(g);
+				panel.setBackground(Color.GREEN);
+				numberOfcorrectLetters += 1;
 			}
 			
 			else{
 				JOptionPane.showMessageDialog(null, "Incorrect");
-				panel.setBackground(r);
+				panel.setBackground(Color.RED);
 			}
 			
+			 totalNumberofLetters += 1;
+			 
+			 if( totalNumberofLetters == 10) {
+				 showTypingSpeed(numberOfcorrectLetters);
+				 System.exit(0);
+			 }
 			
 			//after here is keyReleased
 			currentLetter = generateRandomLetter();
@@ -68,5 +77,17 @@ public class Typing_Tutor implements KeyListener {
 		}
 	    
 	
-
+		Date timeAtStart = new Date();
+	     
+		private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+		    Date timeAtEnd = new Date();
+		    long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+		    long gameInSeconds = (gameDuration / 1000) % 60;
+		    double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+		    int charactersPerMinute = (int) (charactersPerSecond * 60);
+		    JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
+		} 
+		
+		int numberOfcorrectLetters = 0;
+		int totalNumberofLetters = 0;
 }
