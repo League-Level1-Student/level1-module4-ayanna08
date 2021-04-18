961ea0dcd16a473a85324db39e835d1ae4bfe821
root <root@16424db850fb>
2021-04-18 21:30:30 +0000

    calculator

--- src/_08_calculator/Calculator.java
+++ src/_08_calculator/Calculator.java
@@ -1,5 +1,53 @@
 package _08_calculator;
 
-public class Calculator {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
+public class Calculator implements Runnable {
+	public static void main(String[] args) {
+		
+		}
+	void makeButtons(){
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JOptionPane.showInputDialog("Enter two numbers and select if you want them added, subtracted, multiplied, or divided");
+		JButton button1 = new JButton();
+		JButton button2 = new JButton();
+		JButton button3 = new JButton();
+		JButton button4 = new JButton();
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+		
+		int add(int num1, int num2){
+		int sum = num1 + num2;
+		return sum;
+		}
+	
+		int subtraction(int num1, int num2){
+		int sum = num1 - num2;
+		return sum;
+		}
+		
+		int multiplication(int num1, int num2){
+		int sum = num1 * num2;
+		return sum;
+		}
+		
+		int division(int num1, int num2){
+		int sum = num1 / num2;
+		return sum;
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
 }
---
+++ src/_08_calculator/CalculatorRunner.java
@@ -0,0 +1,14 @@
+package _08_calculator;
+
+import javax.swing.SwingUtilities;
+
+
+public class CalculatorRunner {
+public static void main(String[] args) {
+	SwingUtilities.invokeLater(new Calculator());
+ 	 begin();
+}
+static void begin() {
+	
+}
+}
