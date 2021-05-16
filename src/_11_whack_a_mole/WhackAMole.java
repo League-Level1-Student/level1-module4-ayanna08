53bf9801968c3cf47da9e44cac10757f8dc3532c
root <root@4f8b53f09788>
2021-05-16 21:29:26 +0000

    whack a mole

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,27 +1,66 @@
 package _11_whack_a_mole;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton button1 = new JButton();
-	JButton button2 = new JButton();
-	JButton button3 = new JButton();
-	JButton button4 = new JButton();
-	JButton button5 = new JButton();
-	JButton button6 = new JButton();
-	JButton button7 = new JButton();
-	JButton button8 = new JButton();
-	JButton button9 = new JButton();
-	JButton button10 = new JButton();
-	JButton button11= new JButton();
-	JButton button12 = new JButton();
+	Date timeAtStart = new Date();
+	
+	void drawButtons(){
+		frame.add(panel);
+		Random randy = new Random();
+	int number = randy.nextInt(20);
+	
+	for (int i = 0; i < 20; i++) {
+		if(i == number) {
+			JButton button1 = new JButton();
+			button1.setText("MOLE!");
+			button1.addActionListener(this);
+			panel.add(button1);
+		}
+		else {
+			JButton button1 = new JButton();
+			button1.addActionListener(this);
+			panel.add(button1);
+		}
+	}
 	frame.setVisible(true);
 	frame.pack();
 		
 	}
+	int buttonpressed = 0;
+	int molemissed = 0;
+	
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date();
+	    JOptionPane.showMessageDialog(null, "Your whack rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	                  + " moles per second.");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton button3 = (JButton) e.getSource();
+		if(button3.getText().equals("MOLE!")) {
+			buttonpressed += 1;
+		}
+		
+		if(buttonpressed == 10) {
+			endGame(timeAtStart, buttonpressed);
+		}
+		frame.remove(panel);
+		panel = new JPanel();
+		drawButtons();
+	}
+}
 
