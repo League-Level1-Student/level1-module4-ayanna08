7703abe9806228f12a99cc9475c8868a1114a085
root <root@e65de09ed2de>
2021-06-20 21:32:08 +0000

    slot machine

--- src/_12_slot_machine/Slot_Machine.java
+++ src/_12_slot_machine/Slot_Machine.java
@@ -2,25 +2,83 @@ package _12_slot_machine;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Slot_Machine {
+public class Slot_Machine {  
 	
 	JFrame frame = new JFrame();
+	JButton button = new JButton();
+	JPanel panel = new JPanel();
 	JLabel label1;
 	JLabel label2;
-	JLabel label3;
+	JLabel label0;
+
+	void picture() {
+	Random randy = new Random();
+	int number = randy.nextInt(3);
+	int numb = randy.nextInt(3);
+	int num = randy.nextInt(3);
+	try {
+	if(number == 0) {
+	label0 = createLabelImage("cherry.jpg");
+	}
+	
+	if(number == 1) {
+	label0 = createLabelImage("lime.jpg");
+	}
+	
+	if(number == 2) {
+	label0 = createLabelImage("orange.jpg");
+	}
+	
+	if(numb == 0) {
+	label1 = createLabelImage("cherry.jpg");
+	}
+	
+	if(numb == 1) {
+	label1 = createLabelImage("lime.jpg");
+	}
+	
+	if(numb == 2) {
+	label1 = createLabelImage("orange.jpg");
+	}
+	
+	if(num == 0) {
+	label2 = createLabelImage("cherry.jpg");
+	}
+	
+	if(numb == 1) {
+	label2 = createLabelImage("lime.jpg");
+	}
+	
+	if(num == 2) {
+	label2 = createLabelImage("orange.jpg");
+	}
+	
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+	}
+	
 	
 	void makeButtons(){
 		try {
-			label1 = createLabelImage("cherry.jpg");
-			label2 = createLabelImage("lime.jpg");
-			label3 = createLabelImage("orange.jpg");
+			label0 = createLabelImage("cherry.jpg");
+			label1 = createLabelImage("lime.jpg");
+			label2 = createLabelImage("orange.jpg");
 			
+			panel.add(label0);
+			panel.add(label1);
+			panel.add(label2);
+			frame.add(panel);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
